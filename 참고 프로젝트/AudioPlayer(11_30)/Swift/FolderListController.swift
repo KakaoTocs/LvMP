34103c792857462afb7231d825a01ee023e1054d
KakaoTocs <kimjinu5555@gmail.com>
2018-09-19 14:28:44 +0900

    참고프로젝트 버전문제 해결
    Swift2.3 -> Swift4

---
+++ 참고 프로젝트/AudioPlayer(11_30)/Swift/FolderListController.swift
@@ -0,0 +1,35 @@
+//
+//  FolderListController.swift
+//  AudioPlayer
+//
+//  Created by DGSW_TEACHER on 2017. 2. 8..
+//  Copyright © 2017년 dgsw. All rights reserved.
+//
+
+import UIKit
+
+class FolderListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
